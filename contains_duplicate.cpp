ee1ae3174e98f4def4129c1b65169d27a1071bc0
Viggo Xia <viggoxia@outlook.com>
2016-03-08 18:38:14 +0800

    Update contains_duplicate.cpp

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums){
     if(nums.size() <= 1){
-        return 0;
+        return false;
     }
     sort(nums.begin(), nums.end());
     for(int i=0; i<nums.size()-1; ++i){
         if(nums[i] == nums[i+1]){
-            return 1;
+            return true;
         }
     }
-    return 0;
+    return false;
     }
 };
