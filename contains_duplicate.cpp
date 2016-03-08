36afe0fa9992abed42b5c6b45e94bdd13a473b74
Viggo Xia <viggoxia@outlook.com>
2016-03-08 18:37:35 +0800

    Update contains_duplicate.cpp

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -5,7 +5,7 @@ public:
         return 0;
     }
     sort(nums.begin(), nums.end());
-    for(int i = 0; i < nums.size() - 1; ++i){
+    for(int i=0; i<nums.size()-1; ++i){
         if(nums[i] == nums[i+1]){
             return 1;
         }
