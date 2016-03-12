242cc44cc51505089b07522d21daa07754933503
Viggo Xia <viggoxia@outlook.com>
2016-03-12 14:29:05 +0800

    Update 042trapping_rain_water.cpp

--- 042trapping_rain_water.cpp
+++ 042trapping_rain_water.cpp
@@ -5,12 +5,12 @@ public:
         vector<int> max_left(n);         //stores max_lefts;
         vector<int> max_right(n);        //stores max_rights;
         //store max_left from left to right, store max_right from right to left;
-        for(int i=1; i<n; ++i){
+        for(int i=1; i<n; ++i){            //i must start from 1, max_left[0] must be 0;
             max_left[i] = max(max_left[i-1], height[i-1]);
-            max_right[n-1-i] = max(max_right[n-i], height[n-i]);
+            max_right[n-1-i] = max(max_right[n-i], height[n-i]);    //max_right[n-1] must be 0 too;
         }
         int sum = 0, temp;
-        for(int i=0; i<n; ++i){
+        for(int i=0; i<n; ++i){           
             if((temp = min(max_left[i], max_right[i])) > height[i]){
                 sum += temp - height[i];
             }
