b5594468a46ea7c56121bfea18f4b0c003bbaacc
Viggo Xia <viggoxia@outlook.com>
2016-03-15 23:38:45 +0800

    Update 048rotate_image.cpp

--- 048rotate_image.cpp
+++ 048rotate_image.cpp
@@ -5,12 +5,14 @@ public:
 
         for(int i=0; i<n; ++i){
             for(int j=0; j<n-i; ++j){
+                //reverse by semi-diagonal;
                 swap(matrix[i][j], matrix[n-1-j][n-1-i]);
             }
         }
 
         for(int i=0; i<n/2; ++i){
             for(int j=0; j<n; ++j){
+                //reverse by horizonal line;
                 swap(matrix[i][j], matrix[n-1-i][j]);
             }
         }
